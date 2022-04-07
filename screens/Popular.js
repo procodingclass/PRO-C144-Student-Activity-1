f5ba86cba27096fbc2a44d4ac7c7add0f9c7d9aa
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-04-08 03:35:56 +0530

    Add files via upload

---
+++ screens/Popular.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  ImageBackground,
+  Image,
+  Text,
+} from "react-native";
+import axios from "axios";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default class PopularMoviesScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  // write your code here
+
+
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  cardContainer: {
+    borderRadius: RFValue(10),
+    height: RFValue(200),
+    marginHorizontal: RFValue(20),
+    marginVertical: RFValue(15),
+  },
+  posterImage: {
+    flex: 1,
+    borderRadius: RFValue(10),
+  },
+  title: {
+    fontSize: RFValue(15),
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "monospace",
+    marginVertical: RFValue(2),
+  },
+  subtitle: {
+    fontSize: RFValue(10),
+    fontWeight: "bold",
+    color: "white",
+    fontFamily: "monospace",
+    marginVertical: RFValue(2),
+  },
+  movieTitleContainer: {
+    position: "absolute",
+    backgroundColor: "#3c8ed9",
+    opacity: 0.7,
+    padding: RFValue(10),
+    bottom: RFValue(10),
+    left: RFValue(10),
+    borderRadius: RFValue(10),
+  },
+});
